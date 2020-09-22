884cac9670a347d0313bc5e260e12bc3ab372234
Greg Mall <gregmall157@gmail.com>
2020-09-21 17:09:28 -0700

    first app route and test completed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,19 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
+  it('returns a body with plain text hi when we go to the / route', async() => {
+    const response = await request(app)
+      .get('/');
+
+    expect(response.text).toEqual('hi');
+
+
+  });
+  it('returns a status code 200 and plain text with the request body when we go to the /echo route', async() => {
+    const response = await request(app)
+      .post('/echo');
+
+    expect(response.body).toEqual('200 ok');
+  });
 
 });
